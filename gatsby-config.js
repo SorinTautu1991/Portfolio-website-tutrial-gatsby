542d895d8650ea74fd3d09c29d9b141ecf8f9875
SorinTautu1991 <sorin.tautu@gmail.com>
2021-10-28 23:02:28 +0300

    Seo done first part

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,5 +1,14 @@
 module.exports = {
   /* Your site config here */
+  siteMetadata: {
+    title: "WebDev Portfolio",
+    description:
+      "Awesome webdev portfolio project build with Gatsby and Strapi",
+    titleTemplate: `%s | WebDev Portfolio`,
+    url: `https://gracious-brattain-e882f4.netlify.app`,
+    twitterUsername: `@johnSnow_doe`,
+    image: `mainImg.png`,
+  },
 
   plugins: [
     `gatsby-plugin-image`,
